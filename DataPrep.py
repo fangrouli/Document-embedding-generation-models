9d3e03a1b9a50a7b165193c26667849793eda512
FRL <80878559+fangrouli@users.noreply.github.com>
2021-12-16 14:12:42 +0800

    Update DataPrep.py

--- DataPrep.py
+++ DataPrep.py
@@ -1,15 +1,38 @@
+'''
+Import, process of the raw data downloaded from 
+https://hpi.de/naumann/projects/web-science/paar-patent-analysis-and-retrieval/patentmatch.html.
+User will be prompted to enter the directory of train and test dataset. 
+The following directories will be automatically generated and used in other python codes in this repo.
+
+The datasets are then cleared with Regex and stored in easier-to-access .csv files.
+'''
+
 import pandas as pd
 pd.options.mode.chained_assignment = None  #ignore the warning
 from tqdm import tqdm
 import regex as re
 
 def import_train_set(direc):
+    '''
+    Import of datasets.
+    Returns a dataframe that stores the data.
+    
+    @ direc (string): directory of the respective file.
+    '''
+    
     with open(direc) as file:
         content = pd.read_csv(file, sep = '\t')
     file.close()
     return content
 
 def sent_clear(sentence):
+    '''
+    Clearing of data.
+    Returns a cleared version of the paragrpah.
+    
+    @ sentence (string): the paragraph to be cleared.
+    '''
+    
     pat = re.compile(r'(figure\s)|(figs\.)|(fig\.)|(\d[a-zA-Z]\s)|(\s[a-zA-Z]\d)|(\d\.\d)|(\d)')
     sentence = pat.sub(r'', sentence)
 
@@ -22,21 +45,35 @@ def sent_clear(sentence):
     return sentence
 
 def remove_1_sent(df, col):
-  to_be_removed = []
-  for i in tqdm(range(len(df))):
-    sent =  df[col][i]
-    sent_ls = sent.split('.')
+    '''
+    Pre-process of data (removal of instances with just one sentence in their paragraphs).
+    Returns a list of indexes of the instances to be removed.
+    
+    @ df (dataframe): Dataframe that stores the paragraphs.
+    @ col (string): The column name of the Dataframe that stores the data and is to be processed.
+    '''
+    
+    to_be_removed = []
+    for i in tqdm(range(len(df))):
+        sent =  df[col][i]
+        sent_ls = sent.split('.')
 
-    for s in sent_ls.copy():
-      if ' ' not in s or len(s) <= 5:
-        sent_ls.remove(s)
+        for s in sent_ls.copy():
+            if ' ' not in s or len(s) <= 5:
+            sent_ls.remove(s)
 
-    if len(sent_ls) <= 1:
-      to_be_removed.append(i)
-  return to_be_removed
+        if len(sent_ls) <= 1:
+            to_be_removed.append(i)
+    return to_be_removed
 
 
 def cleaning(df, final_dir):
+    '''
+    Pre-process of data, saving the data into a .csv file.
+    
+    @ df (dataframe): Dataframe that stores the paragraphs.
+    @ final_dir (string): the directory of the .csv file to be stored.
+    '''
     text_rem = remove_1_sent(df, 'text_b')
     df.drop(labels = text_rem, axis = 0, inplace = True)
     df.reset_index(inplace = True, drop = True)
